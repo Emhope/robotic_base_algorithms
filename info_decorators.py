72d2ac9fb4ce8b9b166320c85b94c01ed6fd673e
Emhope <michailopalev02@gmail.com>
2024-02-23 00:49:15 +0300

    add info_decorators.py

---
+++ info_decorators.py
@@ -0,0 +1,22 @@
+import time
+import cv2
+
+
+def _timer(func):
+
+    def wrapp(*args, **kwargs):
+        s = time.perf_counter()
+        res = func(*args, **kwargs)
+        print(f'{time.perf_counter() - s} s.')
+        return res    
+    return wrapp
+
+
+def _save_res(func, test_name='test.png'):
+
+    def wrapp(*args, **kwargs):
+        res = func(*args, **kwargs)
+        cv2.imwrite(test_name, res)
+        return res
+    
+    return wrapp
